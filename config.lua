24a2366cd46b185636b91e0640dd3740d8fb8701
One Shine <117028965+wannn28@users.noreply.github.com>
2024-10-20 14:14:09 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,6 @@
 -- config.lua
 Config = {
     framework = 'qbcore', -- Options: 'qbcore', 'esx'
-    emote = 'clipboard',
     FuelResource = 'qb-fuel', -- qb-fuel, ps-fuel
     target = 'qb-target', -- Options: 'qb-target', 'qtarget', 'ox_target', 'drawtext'
     locationped = vector4(-1045.74, -2726.3, 20.17, 326.2),
